f2755f5dd1bd2317cd5455e7e6474a125cf0c0b3
Joha-will <johawilsonatu@icloud.com>
2022-12-05 21:12:01 +0000

    setup admin panel to interact with the booking model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_filter = ('booking_id', 'booked_on', 'no_of_persons')
+    list_display = ('booking_id', 'full_name', 'no_of_persons', 'approved', 'booked_on')
+    search_fields = ('full_name', 'email')
+    actions = ['approve_bookings']
+
+    def approve_bookings(self, request, queryset):
+        queryset.update(approved=True)
