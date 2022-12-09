6d1fb268ce2d3d392ac474c139042e7e35637132
Joha-will <johawilsonatu@icloud.com>
2022-12-09 01:41:46 +0000

    create new app call webpages and connect urls and views to render the home page for index.html and booking.html

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, HttpResponse
+from django.views.generic import (TemplateView)
 from .models import Booking
 
-class BookingListView(ListView):
-    model = Booking
+
+class BookingHome(TemplateView):
     template_name = 'booking.html'
