38257e0ff43204a3e0fd7eec1671ed43e95d81c3
Joha-will <johawilsonatu@icloud.com>
2022-12-16 15:54:25 +0000

    create testing for create_booking and update_booking views

--- bookings/tests_views.py
+++ bookings/tests_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from .models import Booking
 
 class TestViews(TestCase):
 
@@ -14,7 +14,16 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'bookings/booking.html')
 
     def test_create_booking(self):
+        response = self.client.get('/create')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/create-booking.html')
+
     def test_update_booking(self):
+        customer = Booking.objects.create(full_name='active-personel')
+        response = self.client.get(f'/update/{booking_id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/index.html')
+
     def test_delete_booking(self):
     def test_can_create_booking(self):
     def test_can_update_booking(self):
