033dd0a866e8a4490e4bad1d30f7c127f5567732
Joha-will <johawilsonatu@icloud.com>
2022-12-16 15:34:52 +0000

    create test for views.py and test booking_home and index_page

--- bookings/tests_views.py
+++ bookings/tests_views.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_index_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/index.html')
+
+    def test_booking_home(self):
+        response = self.client.get('/booking')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/booking.html')
+
+    def test_create_booking(self):
+    def test_update_booking(self):
+    def test_delete_booking(self):
+    def test_can_create_booking(self):
+    def test_can_update_booking(self):
+    def test_can_delete_booking(self):
