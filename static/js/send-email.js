b1793ca461797c85a426dee12b8b2d6789a49234
Joha-will <johawilsonatu@icloud.com>
2022-12-18 18:31:43 +0000

    add code to email.js to change modal message response and errors and also styled colours

--- static/js/send-email.js
+++ static/js/send-email.js
@@ -7,15 +7,21 @@ function sendMail(contactUs) {
         .then(
             function (response) {
                 if (response.status == 200) {
-                   $(".my-modal").modal('toggle', response)
-                   $('.modal-title').html('Thank you for your email!')
-                   $('.modal-body').html('<p>A member of staff would be in contact with you shortly. Your email/feedbacks mean alot to us here at active-spring.</p>')
+                   $(".my-modal").modal('toggle');
+                   $('.modal-title').html(`Thank you for your email!`);
+                   $('.modal-body').html('<p>A member of staff would be in contact with you shortly. Your email/feedbacks are important to us here at active-spring.</p>');
+                   $("#change-color").addClass("bg-green");
 
 
                 }
             },
             function (error) {
-                console.log('Unsuccessful', error)
+                $(".my-modal").modal('toggle');
+                $('.modal-title').html(`Sorry but an ${error.error} has occured!`);
+                $('.modal-body').html('<p>Please return back to the home page then try again. Alternatively you can contact us at (+0700800100).</p>');
+                $(".modal-header").removeClass("grad");
+                $(".modal-body").removeClass("grad");
+                $("#change-color").addClass("red-btn");
             });
             return false
 }
