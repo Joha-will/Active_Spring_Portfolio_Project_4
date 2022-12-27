39d64dc4b80128e189d0d13d579fd5a51df5ef2b
Joha-will <johawilsonatu@icloud.com>
2022-12-27 17:04:48 +0000

    adjust settings

--- activespring/settings.py
+++ activespring/settings.py
@@ -14,9 +14,10 @@ from pathlib import Path
 
 import os
 import dj_database_url
-from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
+
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +54,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'bookings',
     'webpages',
-    "phonenumber_field",
+    'phonenumber_field',
 ]
 
 SITE_ID = 1
